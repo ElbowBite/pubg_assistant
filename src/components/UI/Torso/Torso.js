beafee7bcc98d8c3da9b5fe69318fae7b56f9243
Vitaliy Idiatov <iadernik@gmail.com>
2019-08-23 09:08:28 +0500

    melee fixed (punches removed)

--- src/components/UI/Torso/Torso.js
+++ src/components/UI/Torso/Torso.js
@@ -7,11 +7,14 @@ import styles from "./Torso.module.css";
 const Torso = props => {
   let helmetSpan = null;
   let vestSpan = null;
-	let restSpan = null;
+  let restSpan = null;
+
 	const [showHtkTooltip, toggleHtkTooltip] = useState(false);
   const [showHtkTooltipButton, toggleHtkTooltipButton] = useState(false);
+  const [currentDmg, setCurrentDmg] = useState(0);
 
-  const onMouseOver = () => {
+  const onMouseOver = (htk) => {
+    setCurrentDmg(htk);
     toggleHtkTooltip(true);
   }
   
@@ -19,7 +22,18 @@ const Torso = props => {
     if (!showHtkTooltipButton) {
       toggleHtkTooltip(false);
     }
-  }
+	}
+	
+	const htkPositioner = htk => {
+		switch (htk) {
+			case "1":	return "-1px";
+			case "2":	return "31px";
+			case "3": return "79px";
+			case "4":	return "127px";
+			case "5": return "174px";
+			default: return "207px";
+		}
+	}
 
   /* Defining base hit area damage multipliers */
   const baseAreaDmgMulitpliers = {
@@ -37,48 +51,56 @@ const Torso = props => {
     Calf: 0.5,
     Foot: 0.3
   };
+
   /* Pulling base damage of a currently selected weapon from state.weapons */
   const curBaseDmg = props.weapons[props.current.weaponType].reduce(
     (pv, cv) => {
       if (cv.name === props.current.weapon) {
         return cv.baseDmg;
       }
+
       return pv;
     },
     ""
   );
+
   /* Pulling fire rate of a currently selected weapon from state.weapons */
   const curFireRate = props.weapons[props.current.weaponType].reduce(
     (pv, cv) => {
       if (cv.name === props.current.weapon) {
         return cv.fireRate;
       }
+
       return pv;
     },
     ""
   );
+
   /* Pulling damage multiplier of a currently selected helmet */
   const curHelmetDmgMultiplier = props.gear.helmets.reduce((pv, cv) => {
     if (cv.name === props.current.helmet) {
       return cv.dmgMulitplier;
     }
+
     return pv;
   }, "");
+
   /* Pulling damage multiplier of a currently selected vest */
   const curVestDmgMultiplier = props.gear.vests.reduce((pv, cv) => {
     if (cv.name === props.current.vest) {
       return cv.dmgMulitplier;
     }
+
     return pv;
   }, "");
+
   /* Damage w/o armor multiplier */
   const damagBeforeArmor = (
     areaMultiplier,
     baseAreaMultiplier,
     weapBaseDmg
-  ) => {
-    return areaMultiplier * baseAreaMultiplier * weapBaseDmg;
-  };
+  ) => areaMultiplier * baseAreaMultiplier * weapBaseDmg;
+
   /* Checking if current weapon is a crossbow and needs its own dmg multiplier to be used */
   let multiplierToUse = null;
   if (props.current.weapon === "Crossbow") {
@@ -86,6 +108,7 @@ const Torso = props => {
   } else {
     multiplierToUse = props.current.weaponType;
   }
+
   /* Forming list of spans with damage dealt to area */
   const dmgAreas = props.dmgMulitpliers[multiplierToUse].map(multiplier => {
     /* Calculating damage before armor */
@@ -94,14 +117,18 @@ const Torso = props => {
       baseAreaDmgMulitpliers[multiplier.name],
       curBaseDmg
     ).toFixed(1);
+
     /* Calculating damage with helmet multiplier */
     const dmgWithHelmet = (dmgBeforeArmor * curHelmetDmgMultiplier).toFixed(1);
+
     /* Calculating damage with vest multiplier */
     const dmgWithVest = (dmgBeforeArmor * curVestDmgMultiplier).toFixed(1);
+
     /* Calculating number of hits to kill */
     const htkBeforeArmor = Math.ceil(100 / dmgBeforeArmor).toString(10);
     const htkWithHelmet = Math.ceil(100 / dmgWithHelmet).toString(10);
     const htkWithVest = Math.ceil(100 / dmgWithVest).toString(10);
+
     /* Defining what to show on torso */
     if (props.current.weaponType === "SGs") {
       const curNumberOfPellets = props.weapons[props.current.weaponType].reduce(
@@ -109,10 +136,12 @@ const Torso = props => {
           if (cv.name === props.current.weapon) {
             return cv.numberOfPellets;
           }
+
           return pv;
         },
         ""
       );
+
       if (props.current.view === "DMG") {
         helmetSpan = (
           <>
@@ -120,12 +149,14 @@ const Torso = props => {
             <span className={styles.Pellets}>x{curNumberOfPellets}</span>
           </>
         );
+
         vestSpan = (
           <>
             {dmgWithVest}
             <span className={styles.Pellets}>x{curNumberOfPellets}</span>
           </>
         );
+
         restSpan = (
           <>
             {dmgBeforeArmor}
@@ -138,6 +169,7 @@ const Torso = props => {
       vestSpan = dmgWithVest;
       restSpan = dmgBeforeArmor;
     }
+
     if (props.current.view === "HTK") {
       if (props.current.weaponType === "SGs") {
         const curNumberOfPellets = props.weapons[
@@ -148,18 +180,21 @@ const Torso = props => {
           }
           return pv;
         }, "");
+
         helmetSpan = (
           <>
             {htkWithHelmet}
             <span className={styles.Pellets}>/{curNumberOfPellets}</span>
           </>
         );
+
         vestSpan = (
           <>
             {htkWithVest}
             <span className={styles.Pellets}>/{curNumberOfPellets}</span>
           </>
         );
+
         restSpan = (
           <>
             {htkBeforeArmor}
@@ -172,6 +207,7 @@ const Torso = props => {
         restSpan = htkBeforeArmor;
       }
     }
+
     if (props.current.view === "TTK") {
       if (props.current.weaponType === "SGs") {
         helmetSpan = (Math.floor(htkWithHelmet / 9) * curFireRate).toFixed(2) + "s";
@@ -182,7 +218,8 @@ const Torso = props => {
         vestSpan = ((htkWithVest - 1) * curFireRate).toFixed(2) + "s";
         restSpan = ((htkBeforeArmor - 1) * curFireRate).toFixed(2) + "s";
       }
-		}
+    }
+
     /* Outputing spans with damage */
     switch (multiplier.name) {
       case "Head":
@@ -190,7 +227,7 @@ const Torso = props => {
         return (
           <p
 						key={multiplier.name}
-						onMouseOver={onMouseOver}
+						onMouseOver={() => onMouseOver(htkWithHelmet)}
 						onMouseOut={onMouseOut}
             className={classNames(
 							styles[multiplier.name],
@@ -209,7 +246,7 @@ const Torso = props => {
         return (
           <p
 						key={multiplier.name}
-						onMouseOver={onMouseOver}
+						onMouseOver={() => onMouseOver(htkWithVest)}
 						onMouseOut={onMouseOut}
             className={classNames(
 							styles[multiplier.name],
@@ -224,7 +261,7 @@ const Torso = props => {
         return (
           <p
 						key={multiplier.name}
-						onMouseOver={onMouseOver}
+						onMouseOver={() => onMouseOver(htkBeforeArmor)}
 						onMouseOut={onMouseOut}
             className={classNames(
 							styles[multiplier.name],
@@ -253,6 +290,7 @@ const Torso = props => {
 			<div className={!showHtkTooltip && styles.Hidden}>
 				<p>Hits to kill:</p>
 				<div className={styles.HtkRainbow}>
+          <img src={require("../../../assets/images/vectorpaint.svg")} alt="arrow flew somewhere else" className={styles.arrow} style={{ marginLeft: htkPositioner(currentDmg) }} />
           <p className={styles.HtkRainbowSides}>1</p>
           {[2, 3, 4, 5].map(num => (
             <p className={styles.HtkRainbowItem}>{num}</p>
@@ -264,13 +302,11 @@ const Torso = props => {
 	);
 };
 
-const mapStateToProps = state => {
-  return {
-    dmgMulitpliers: state.dmgMulitpliers,
-    current: state.current,
-    weapons: state.weapons,
-    gear: state.gear
-  };
-};
+const mapStateToProps = state => ({
+  dmgMulitpliers: state.dmgMulitpliers,
+  current: state.current,
+  weapons: state.weapons,
+  gear: state.gear
+});
 
 export default connect(mapStateToProps)(Torso);
